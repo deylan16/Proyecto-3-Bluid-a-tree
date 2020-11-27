3621e677fb60915b9c1c2287174665a52f1d6d01
Joel Gómez <62965696+Joel-Araya@users.noreply.github.com>
2020-11-26 21:54:51 -0600

    Se mejoró la movilidad del personaje, se añadieron animaciones y doble salto

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,34 +6,110 @@ public class PlayerMove : MonoBehaviour
 {
 
     public float runSpeed = 2;
-    public float jumpSpeed = 3;
+    public float jumpSpeed = 4;
     Rigidbody2D rb2D;
+
+    public bool betterJump = true;
+    public float fallMultiplier = 0.5f;
+    public float lowJumpMultiplier = 1f;
+
+    public float doubleJumpSpeed = 3f;
+    private bool canDoubleJump;
+
+    public SpriteRenderer  spriteRenderer;
+    public Animator animator;  
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+
+    public void Update()
+    {
+
+        if (Input.GetKey("space"))
+        {
+            if (CheckGround.isGrounded)
+            {
+                canDoubleJump = true;
+                rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+            }
+
+            else
+            {
+                if(Input.GetKeyDown("space"))
+                {
+                    if (canDoubleJump)
+                    {
+                        animator.SetBool("DoubleJump", true);
+                        rb2D.velocity = new Vector2(rb2D.velocity.x, doubleJumpSpeed);
+                        canDoubleJump = false; 
+
+                    }
+                }
+            }
+        }
+
+        if (CheckGround.isGrounded == false)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+        }
+
+        if (CheckGround.isGrounded == true)
+        {
+            animator.SetBool("Jump", false);
+            animator.SetBool("DoubleJump", false);
+            animator.SetBool("Falling", false);
+        }
+
+        if (rb2D.velocity.y < 0) 
+        {
+            animator.SetBool("Falling", true);
+        }
+
+        else if (rb2D.velocity.y > 0)
+        {
+            animator.SetBool("Falling", false);
+
+        }
+
+    }
+
+    void FixedUpdate()
     {
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Run", true);
         }
 
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Run", true);
         }
 
         else
         {
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            animator.SetBool("Run", false);
+
         }
 
-        if (Input.GetKey("space") && CheckGround.isGrounded)
+        if (betterJump)
         {
-            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+            if (rb2D.velocity.y < 0)
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+            }
+            if (rb2D.velocity.y > 0 && !Input.GetKey("space"))
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+            }
         }
     }
 }

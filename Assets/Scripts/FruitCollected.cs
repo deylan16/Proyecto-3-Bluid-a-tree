3e4ce43362256fb775a3e462bda5c5427cd0b923
Armando García Paniagua <62965639+ArmandGP07@users.noreply.github.com>
2020-11-29 21:59:20 -0600

    Objetos Aleatorios
    Se agregó la posibilidad de poder generar objetos aleatorios para el arbol (sujeto al cambio correspondiente)

--- Assets/Scripts/FruitCollected.cs
+++ Assets/Scripts/FruitCollected.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class FruitCollected : MonoBehaviour
 {
-    Rigidbody2D rb2D;
+    public Rigidbody2D rb2D;
+    public bool isInGround = false;
 
     void Start()
     {
@@ -18,11 +19,13 @@ public class FruitCollected : MonoBehaviour
             GetComponent<SpriteRenderer>().enabled = false;
             gameObject.transform.GetChild(0).gameObject.SetActive(true);
             Destroy(gameObject, 0.5f);
+            TokenCreation.Destroy(this, 0.5f);
         }
 
         if (collision.CompareTag("Ground"))
         {
             rb2D.constraints = RigidbodyConstraints2D.FreezeAll;
+            isInGround = true;
 
         }
     }
---
+++ Assets/Scripts/TokenCreation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TokenCreation : MonoBehaviour
+{
+    public GameObject token;
+    public static GameObject[] tokenEnPantalla;
+    public static int limit = 4;
+
+    private void Start()
+    {
+        tokenEnPantalla = GameObject.FindGameObjectsWithTag("Token");
+        InvokeRepeating("UpdateList", 0.0f, 0.01f);
+        InvokeRepeating("SpawnToken", 5.0f, 20.0f);
+        
+    }
+
+    void SpawnToken()
+    {
+        Debug.Log(tokenEnPantalla.Length);
+        if (tokenEnPantalla.Length < limit)
+        {
+
+            Vector2 randomToken = new Vector2(Random.Range(-1.9f, 1.9f), 1.2f);
+            Instantiate(token, randomToken, transform.rotation);
+
+ 
+        }
+
+    }
+
+    void UpdateList()
+    {
+        tokenEnPantalla = GameObject.FindGameObjectsWithTag("Token");
+    }
+}
+
